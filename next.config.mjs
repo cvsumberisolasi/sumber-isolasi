8e9d2fe976db75f62cb255fa1439dff115dc9b2b
Nyai Tarsih <ntarsih32@gmail.com>
2025-11-02 02:47:35 +0000

    Try fixing this error: `Runtime Error: Body exceeded 1 MB limit. To conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,8 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  serverActions: {
+    bodySizeLimit: '10mb',
   },
 };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
